0c58b2969f379c27d38715b5d753599983b05683
8Kit <kit@made.llc>
2024-07-02 02:48:25 -0400

    ln

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,7 +7,7 @@ const HeaderSocials = () => {
   return (
     <div className='header__socials'>
         <a href="https://github.com/dev-kitt/" target="_blank" rel="noopener noreferrer"><AiFillGithub /></a>
-        <a href="https://www.linkedin.com/in/qakit/" target="_blank" rel="noopener noreferrer"><AiFillLinkedin /></a>
+        <a href="https://www.linkedin.com/in/qakitt/" target="_blank" rel="noopener noreferrer"><AiFillLinkedin /></a>
         <a href="https://community.smartbear.com/t5/user/viewprofilepage/user-id/110153" target="_blank" rel="noopener noreferrer"><FaMicroblog /></a>
     </div>
   )
